ac253dcb6b2916b60514e4f541fdbc2e99b4b0e1
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2026-04-15 15:04:43 +0100

    Base off Fedora Kinoite, Install LibreOffice and Virtualbox (#5)
    * Install Linux kernel
    
    * Update Containerfile
    
    * Use full Void base instead of container base
    
    * Link kernel to correct place
    
    * Fedora :(
    
    * Greatest name of all time
    
    * Try aurora
    
    * CoreOS
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update Containerfile
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,8 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
 
 # Use a COPR Example:
 #
@@ -18,7 +11,9 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 -y install https://download.virtualbox.org/virtualbox/7.2.6/VirtualBox-7.2-7.2.6_172322_fedora40-1.x86_64.rpm
+dnf5 -y install libreoffice
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
